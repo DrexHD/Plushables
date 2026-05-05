5ca1e98a8de0837e0c1dbd368b2b78ad17cc9a78
khazoda <khazoda@gmail.com>
2026-05-05 17:30:08 +0100

    merge critical mod collision fix from baseline

--- fabric/src/main/java/com/khazoda/plushables/platform/FabricConfigSyncClient.java
+++ fabric/src/main/java/com/khazoda/plushables/platform/FabricConfigSyncClient.java
@@ -2,7 +2,7 @@ package com.khazoda.plushables.platform;
 
 import com.khazoda.plushables.PlushablesCommon;
 import com.khazoda.plushables.Constants;
-import com.khazoda.baseline.FabricConfigSync;
+import com.khazoda.baseline.KhazConfigSyncFabric;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
@@ -10,7 +10,7 @@ import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 public class FabricConfigSyncClient implements ClientModInitializer {
   @Override
   public void onInitializeClient() {
-    FabricConfigSync.registerClientboundPayloadType(Constants.CONFIG_SYNC);
+    KhazConfigSyncFabric.registerClientboundPayloadType(Constants.CONFIG_SYNC);
     registerServerConfigReceiver();
     registerDisconnectReloadListener();
   }
