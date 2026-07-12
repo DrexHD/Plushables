19db4afdcff837bf7a626faed94091e4657dba89
khazoda <khazoda@gmail.com>
2026-07-12 17:11:50 +0100

    take out fabric config sync setup as it's included in khazodacore now

--- fabric/src/main/java/com/khazoda/plushables/platform/PlushablesFabricClient.java
+++ fabric/src/main/java/com/khazoda/plushables/platform/PlushablesFabricClient.java
@@ -1,16 +1,11 @@
 package com.khazoda.plushables.platform;
 
-import com.khazoda.core.config.KhazConfigSyncFabric;
 import com.khazoda.core.keybind.KhazKeybindFabric;
-import com.khazoda.plushables.Constants;
-import com.khazoda.plushables.PlushablesConfig;
 import com.khazoda.plushables.PlushablesKeybinds;
 import com.khazoda.plushables.client.model.PlushableOrientationModel;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.model.loading.v1.ModelLoadingPlugin;
 import net.fabricmc.fabric.api.client.model.loading.v1.ModelModifier;
-import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
-import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.minecraft.client.renderer.block.dispatch.BlockStateModel;
 import net.minecraft.world.level.block.Block;
 
@@ -18,14 +13,6 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public final class PlushablesFabricClient implements ClientModInitializer {
-  private static void registerServerConfigReceiver() {
-    ClientPlayNetworking.registerGlobalReceiver(Constants.CONFIG_SYNC.type(), (payload, context) -> PlushablesConfig.CONFIG.applyServerSyncedValues(payload.serverValues()));
-  }
-
-  private static void registerDisconnectReloadListener() {
-    ClientPlayConnectionEvents.DISCONNECT.register((listener, client) -> PlushablesConfig.CONFIG.clearServerSyncedValuesAndReload());
-  }
-
   private static void registerPlushableModelOrientation() {
     ModelLoadingPlugin.register(context -> {
       Map<Block, Map<Integer, BlockStateModel>> cache = new ConcurrentHashMap<>();
@@ -37,9 +24,6 @@ public final class PlushablesFabricClient implements ClientModInitializer {
   @Override
   public void onInitializeClient() {
     registerPlushableModelOrientation();
-    KhazConfigSyncFabric.registerClientboundPayloadType(Constants.CONFIG_SYNC);
-    registerServerConfigReceiver();
-    registerDisconnectReloadListener();
     PlushablesKeybinds.register();
     KhazKeybindFabric.init();
   }
